f0ab0a1371888a36b340f4a77d73069d23f9b290
heimenkyou <wenbin.lo@outlook.com>
2026-08-06 17:01:23 +0800

    chore(frontend): 移除未使用组件声明

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -28,7 +28,6 @@ declare module 'vue' {
     ElCarouselItem: typeof import('element-plus/es')['ElCarouselItem']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCol: typeof import('element-plus/es')['ElCol']
-    ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDescriptions: typeof import('element-plus/es')['ElDescriptions']
     ElDescriptionsItem: typeof import('element-plus/es')['ElDescriptionsItem']
